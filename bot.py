3194b5f0995cc09ab01f55f272473759171abcc3
Solo Coder <kisliydanecka@gmail.com>
2024-07-03 04:35:42 +0300

    dsdsd

--- bot.py
+++ bot.py
@@ -5,6 +5,6 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "YA SHALAVA")
+    bot.reply_to(message, "Hello")
 
 bot.polling()
