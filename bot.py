db7993bd75046aae37d8704fa85880c1f6904a9e
Solo Coder <kisliydanecka@gmail.com>
2024-07-03 04:49:40 +0300

    start

--- bot.py
+++ bot.py
@@ -5,6 +5,19 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Hello")
+    inline_keyboard = telebot.types.InlineKeyboardMarkup()
+    button1 = telebot.types.InlineKeyboardButton('Я заказчик', callback_data='order')
+    button2 = telebot.types.InlineKeyboardButton('Я исполнитель', callback_data='executor')
+    inline_keyboard.add(button1, button2)
+    bot.send_message(message.chat.id, "Кто ты, воин?", reply_markup=inline_keyboard)
 
-bot.polling()
+@bot.callback_query_handler(func=lambda call: True)
+def query_handler(call):
+    text = 'Вы выбрали: '
+    if call.data == 'order':
+        text += 'Я заказчик'
+    elif call.data == 'executor':
+        text += 'Я исполнитель'
+    bot.answer_callback_query(callback_query_id=call.id, text=text, show_alert=False)
+
+bot.polling(none_stop=True)
