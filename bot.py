40ba490c320934fb3b3e4665b95328c06c4cb768
Solo Coder <kisliydanecka@gmail.com>
2024-07-03 04:23:55 +0300

    start

--- bot.py
+++ bot.py
@@ -1,1 +1,10 @@
-xzZXzzxxzaxz
+import telebot
+from config import TOKEN
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Здарова заебал!")
+
+bot.polling()
